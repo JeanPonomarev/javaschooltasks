4647def9cf21720ec300c982dca03aef7513c3d4
JeanPonomarev <jean1995sib@gmail.com>
2020-04-19 13:28:46 +0300

    Second task - Pyramid has been done successfully

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,6 +1,8 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 
 public class PyramidBuilder {
 
@@ -13,9 +15,45 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+        verifyPyramidBuildingPossibility(inputNumbers);
+
+        Collections.sort(inputNumbers);
+
+        ListIterator<Integer> iterator = inputNumbers.listIterator(inputNumbers.size());
+
+        int[][] pyramid = createEmptyMatrix(inputNumbers.size());
+
+        for (int i = pyramid.length - 1; i >= 0; i--) {
+            int j = i + 1;
+            int k = i + pyramid.length - 1;
+
+            while (j > 0) {
+                pyramid[i][k] = iterator.previous();
+
+                j--;
+                k = k - 2;
+            }
+        }
+
+        return pyramid;
     }
 
+    private static <T> void verifyPyramidBuildingPossibility(List<T> list) {
+        if (list.contains(null)) {
+            throw new CannotBuildPyramidException();
+        }
 
+        double root = (-1 + Math.sqrt(1 + 8 * list.size())) / 2.0;
+
+        if (root % 1 != 0) {
+            throw new CannotBuildPyramidException();
+        }
+    }
+
+    private static int[][] createEmptyMatrix(int elementsCount) {
+        int columnsCount = (int) ((-1 + Math.sqrt(1 + 8 * elementsCount)) / 2.0);
+        int rowsCount = 2 * columnsCount - 1;
+
+        return new int[columnsCount][rowsCount];
+    }
 }
